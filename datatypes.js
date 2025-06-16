577a6a5fb17fcbd470b53a35bd98bcfe92745893
Aryan009-sr <Prakharsingh9532@gmail.com>
2025-06-16 11:39:13 +0530

    datatypes file created

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+// first datatype is Number
+let x = 10;
+console.log(typeof(x));
+
+
+// second datatype would be string
+let name = "prakhar singh"
+console.log(typeof(name));
+
+// third datatype is boolean
+let b = true;
+console.log(typeof(b));
+
+// fourth datatype is null
+let n = null;
+console.log(typeof(n));
+
+//fifth datatype is undefined
+let d;
+console.log(typeof(d));
+
+//sixth datatype is bigint
+let bint = BigInt(454542343423535258584985745454759847598749857498);
+console.log(typeof(bint))
