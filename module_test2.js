e9026e5616b446d3d1b746835fd126502de3cf3b
Aryan009-sr <Prakharsingh9532@gmail.com>
2025-06-20 15:59:53 +0530

    function execution part

---
+++ module_test2.js
@@ -0,0 +1,42 @@
+// let str = "Hello World";
+// console.log(str.replace("World", "Universe"));
+
+
+// let str1 = "Hello World";
+// console.log(str1.substr(6));
+
+// let str2 = "Hello";
+// console.log(str.charAt(2));
+
+// // {
+// //     let r = 10;
+// //     console.log(r);
+// // }
+// // console.log(r);
+
+// let str5 = "Hello World";
+// console.log(str5.slice(-5));
+
+// const greet = (name = "Guest") => `Hello ${name}`;
+// console.log(greet('john'));
+// console.log(greet());
+
+// let fruitBasket  = "apple, apple, orange, apple";
+// console.log(fruitBasket.replaceAll('apple','banana')); 
+
+var z = 10;
+x();
+y();
+console.log(z);
+
+function x () {
+    var z = 20;
+    console.log(z);
+}
+
+function y() {
+    var z = 30;
+    console.log(z);
+}
+
+
